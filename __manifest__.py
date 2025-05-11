3e822ad1490b8941aeb625ff1bb5a284fc15527c
Antonio Queb <cardstillo@gmail.com>
2025-05-10 20:11:09 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'CRM Custom Fields',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/CRM',
+    'summary': 'Añade campos personalizados en oportunidades CRM.',
+    'author': 'Alphaqueb Consulting',
+    'depends': ['crm', 'uom'],
+    'data': [
+        'views/crm_lead_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ models/crm_lead.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    service_frequency = fields.Char(string="Frecuencia del Servicio")
+    residue_line_ids = fields.One2many('crm.lead.residue', 'lead_id', string="Listado de Residuos")
+
+class CrmLeadResidue(models.Model):
+    _name = 'crm.lead.residue'
+    _description = 'Residuo cotizado'
+
+    lead_id = fields.Many2one('crm.lead', string="Lead/Oportunidad", required=True, ondelete='cascade')
+    name = fields.Char(string="Residuo", required=True)
+    volume = fields.Float(string="Volumen", required=True)
+    uom_id = fields.Many2one('uom.uom', string="Unidad de Medida", required=True)
