70e4cf6c319c3cf246425a58f46399af633582c8
Antonio Queb <cardstillo@gmail.com>
2025-05-10 21:31:13 -0600

    Up

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -7,8 +7,7 @@ class CrmLead(models.Model):
     residue_line_ids = fields.One2many('crm.lead.residue', 'lead_id', string="Listado de Residuos")
     residue_new = fields.Boolean(string="¿Residuo Nuevo?")
     show_sample_alert = fields.Boolean(string="Mostrar alerta de muestra", compute="_compute_show_sample_alert")
-    sample_result_file = fields.Binary(string="Archivo de Resultados de Muestra")
-    sample_result_filename = fields.Char(string="Nombre del Archivo de Resultados de Muestra")
+    sample_result_ids = fields.One2many('crm.lead.sample.result', 'lead_id', string="Resultados de Muestras")
 
     @api.depends('residue_new')
     def _compute_show_sample_alert(self):
@@ -19,7 +18,17 @@ class CrmLeadResidue(models.Model):
     _name = 'crm.lead.residue'
     _description = 'Residuo cotizado'
 
-    lead_id = fields.Many2one('crm.lead', string="Lead/Oportunidad", required=True, ondelete='cascade')
-    name = fields.Char(string="Residuo", required=True)
-    volume = fields.Float(string="Volumen", required=True)
-    uom_id = fields.Many2one('uom.uom', string="Unidad de Medida", required=True)
+    lead_id = fields.Many2one('crm.lead', required=True, ondelete='cascade')
+    name = fields.Char(required=True)
+    volume = fields.Float(required=True)
+    uom_id = fields.Many2one('uom.uom', required=True)
+
+class CrmLeadSampleResult(models.Model):
+    _name = 'crm.lead.sample.result'
+    _description = 'Resultado de Muestra'
+
+    lead_id = fields.Many2one('crm.lead', required=True, ondelete='cascade')
+    name = fields.Char(string="Descripción del archivo", required=True)
+    file = fields.Binary(string="Archivo", required=True)
+    filename = fields.Char(string="Nombre del Archivo")
+    upload_date = fields.Datetime(string="Fecha de Subida", default=fields.Datetime.now)
