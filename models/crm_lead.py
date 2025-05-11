cb68b6311c598a74757e76e2d437a90e635f6cfc
Antonio Queb <cardstillo@gmail.com>
2025-05-10 21:42:01 -0600

    Up

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -6,15 +6,9 @@ class CrmLead(models.Model):
     service_frequency = fields.Char(string="Frecuencia del Servicio")
     residue_line_ids = fields.One2many('crm.lead.residue', 'lead_id', string="Listado de Residuos")
     residue_new = fields.Boolean(string="¿Residuo Nuevo?")
-    show_sample_alert = fields.Boolean(
-        string="Mostrar alerta de muestra",
-        compute="_compute_show_sample_alert"
-    )
-    sample_result_ids = fields.One2many(
-        'crm.lead.sample.result',
-        'lead_id',
-        string="Resultados de Muestras"
-    )
+    show_sample_alert = fields.Boolean(string="Mostrar alerta de muestra", compute="_compute_show_sample_alert")
+    sample_result_file = fields.Binary(string="Archivo de Resultados de Muestra")
+    sample_result_filename = fields.Char(string="Nombre del Archivo de Resultados de Muestra")
 
     @api.depends('residue_new')
     def _compute_show_sample_alert(self):
@@ -25,20 +19,7 @@ class CrmLeadResidue(models.Model):
     _name = 'crm.lead.residue'
     _description = 'Residuo cotizado'
 
-    lead_id = fields.Many2one('crm.lead', required=True, ondelete='cascade')
-    name = fields.Char(required=True)
-    volume = fields.Float(required=True)
-    uom_id = fields.Many2one('uom.uom', required=True)
-
-class CrmLeadSampleResult(models.Model):
-    _name = 'crm.lead.sample.result'
-    _description = 'Resultado de Muestra'
-
-    lead_id = fields.Many2one('crm.lead', required=True, ondelete='cascade')
-    name = fields.Char(string="Descripción del archivo", required=True)
-    file = fields.Binary(string="Archivo", required=True)
-    filename = fields.Char(string="Nombre del Archivo")
-    upload_date = fields.Datetime(
-        string="Fecha de Subida",
-        default=fields.Datetime.now
-    )
+    lead_id = fields.Many2one('crm.lead', string="Lead/Oportunidad", required=True, ondelete='cascade')
+    name = fields.Char(string="Residuo", required=True)
+    volume = fields.Float(string="Volumen", required=True)
+    uom_id = fields.Many2one('uom.uom', string="Unidad de Medida", required=True)
